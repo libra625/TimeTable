dcc19b1cf3af6840606a27995dab2b5b5df5bdf7
libra625 <18.12.17.moyo@gmail.com>
2026-09-10 19:03:28 +0300

    raw

---
+++ script.js
@@ -0,0 +1,12 @@
+const toggleBtn = document.querySelector('.toggle-week-btn');
+const currentWeek = document.querySelector('.current-week');
+
+toggleBtn.addEventListener('click', () => {
+    document.body.classList.toggle('odd-week-active');
+
+    if (document.body.classList.contains('odd-week-active')) {
+        currentWeek.textContent = 'Displays: odd week';
+    } else {
+        currentWeek.textContent = 'Displays: even week';
+    }
+});
